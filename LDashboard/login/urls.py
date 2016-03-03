43cfd92f97996fe0aeb640d53c199cb164aeb838
LDAR2011 <ldar2011@sina.com>
2016-03-03 11:05:09 +0800

    #del all#

--- LDashboard/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns  
-urlpatterns = [
-    url(r"^$", views.login, name='login'),
-]
-
-urlpatterns += staticfiles_urlpatterns() 
--- LDashboard/login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-# Create your views here.
-
-def login(request):
-    return render(request, 'login/login.html', {})
-
