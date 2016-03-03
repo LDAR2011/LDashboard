022d48b4a13b80d5bfb4fdfad5876711bc70a2fa
LDAR2011 <ldar2011@sina.com>
2016-03-03 17:21:23 +0800

    #integrate all#

--- UserManage/views/permission.py
+++ UserManage/views/permission.py
@@ -22,12 +22,14 @@ def PermissionVerify():
 
             if not iUser.is_superuser: #判断用户如果是超级管理员则具有所有权限
                 if not iUser.role: #如果用户无角色，直接返回无权限
+                    print 'no role'
                     return HttpResponseRedirect(reverse('permissiondenyurl'))
 
                 role_permission = RoleList.objects.get(name=iUser.role)
                 role_permission_list = role_permission.permission.all()
-
+                print 'role_permission_list', role_permission_list
                 matchUrl = []
+                print request.path.rstrip('/')
                 for x in role_permission_list:
                     if request.path == x.url or request.path.rstrip('/') == x.url: #精确匹配，判断request.path是否与permission表中的某一条相符
                         matchUrl.append(x.url)
